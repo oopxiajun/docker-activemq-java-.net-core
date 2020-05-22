d90fbcc2fb4a58851247802d246277cda7763eb6
xiajun <674008411@qq.com>
2020-05-22 10:31:27 +0800

    .net

---
+++ ActiveMQ_Docker_NetCore/ActiveMQ_Consumer/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS.Util;
+namespace ActiveMQ_Consumer
+{
+    class Program
+    {
+        static string connString = "activemq:tcp://192.168.134.139:61616";
+        static string user = "xiajun";
+        static string pwd = "1234@abc";
+        static string queueName = "xiajun_test_queueName";
+        static string topicName = "xiajun_test_topicName";
+        static void Main(string[] args)
+        {
+            TestTopic();
+        }
+
+
+        /// <summary>
+        /// 队列测试
+        /// </summary>
+        /// <param name="args"></param>
+        static void TestTopic()
+        {
+            Console.Title = "主题--消费者1";
+
+            //消费者
+            System.Threading.Tasks.Task.Run(() =>
+            {
+                Uri _uri = new Uri(String.Concat(connString));
+                IConnectionFactory factory = new ConnectionFactory(_uri);
+                using (IConnection conn = factory.CreateConnection(user, pwd))
+                {
+                    using (ISession session = conn.CreateSession())
+                    {
+                        conn.Start();
+                        IDestination destination = SessionUtil.GetTopic(session, topicName);
+                        using (IMessageConsumer consumer = session.CreateConsumer(destination))
+                        {
+                            consumer.Listener += (IMessage message) =>
+                            {
+                                ITextMessage msg = (ITextMessage)message;
+                                Console.WriteLine("接收消息：" + msg.Text);
+                            };
+                            Console.ReadLine();
+                        }
+                    }
+                }
+            });
+
+            Console.ReadLine();
+
+        }
+
+        /// <summary>
+        /// 队列测试
+        /// </summary>
+        /// <param name="args"></param>
+        static void TestQueue()
+        {
+            Console.Title = "队列--消费者2";
+
+            //消费者
+            System.Threading.Tasks.Task.Run(() =>
+            {
+                Uri _uri = new Uri(String.Concat(connString));
+                IConnectionFactory factory = new ConnectionFactory(_uri);
+                using (IConnection conn = factory.CreateConnection(user, pwd))
+                {
+                    using (ISession session = conn.CreateSession())
+                    {
+                        conn.Start();
+                        IDestination destination = SessionUtil.GetQueue(session, queueName);
+                        using (IMessageConsumer consumer = session.CreateConsumer(destination))
+                        {
+                            consumer.Listener += (IMessage message) =>
+                            {
+                                ITextMessage msg = (ITextMessage)message;
+                                Console.WriteLine("接收消息：" + msg.Text);
+                            };
+                            Console.ReadLine();
+                        }
+                    }
+                }
+            });
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ActiveMQ_Docker_NetCore/ActiveMQ_Producer/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS.Util;
+namespace ActiveMQ_Producer
+{
+    class Program
+    {
+        static string connString = "activemq:tcp://192.168.134.139:61616";
+        static string user = "xiajun";
+        static string pwd = "1234@abc";
+        static string queueName = "xiajun_test_queueName";
+        static string topicName = "xiajun_test_topicName";
+
+        static void Main(string[] args)
+        {
+            TestTopic();
+        }
+        /// <summary>
+        /// 测试主题
+        /// </summary>
+        static void TestTopic()
+        {
+
+            Console.Title = "主题--生产者1";
+
+
+            //生产者
+            var __uri = new Uri(string.Concat(connString));
+            IConnectionFactory _factory = new ConnectionFactory(__uri);
+            using (IConnection _conn = _factory.CreateConnection(user, pwd))
+            {
+                using (ISession _session = _conn.CreateSession())
+                {
+                    IDestination _destination = SessionUtil.GetTopic(_session, topicName);
+                    using (IMessageProducer producer = _session.CreateProducer(_destination))
+                    {
+                        //可以写入字符串，也可以是一个xml字符串等
+                        while (true)
+                        {
+                            Console.WriteLine("请输入主题内容:");
+                            string context = Console.ReadLine();
+                            ITextMessage request = _session.CreateTextMessage(context);
+                            producer.Send(request);
+                            Console.WriteLine("发送新新主题：" + context);
+                            System.Threading.Thread.Sleep(200);
+
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 测试队列
+        /// </summary>
+        static void TestQueue()
+        {
+            Console.Title = "队列--生产者1";
+
+
+            //生产者
+            var __uri = new Uri(string.Concat(connString));
+            IConnectionFactory _factory = new ConnectionFactory(__uri);
+            using (IConnection _conn = _factory.CreateConnection(user, pwd))
+            {
+                using (ISession _session = _conn.CreateSession())
+                {
+                    IDestination _destination = SessionUtil.GetQueue(_session, queueName);
+                    using (IMessageProducer producer = _session.CreateProducer(_destination))
+                    {
+                        //可以写入字符串，也可以是一个xml字符串等
+                        while (true)
+                        {
+                            Console.WriteLine("请输入队列内容:");
+                            string context = Console.ReadLine();
+                            ITextMessage request = _session.CreateTextMessage(context);
+                            producer.Send(request);
+                            Console.WriteLine("发送新新队列信息：" + context);
+                            System.Threading.Thread.Sleep(200);
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
